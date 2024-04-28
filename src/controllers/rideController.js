bbcaf7fd21a1567fc666eb1050aa920e6a2fa7c6
Theophilus <theophilusadesola002@gmail.com>
2024-04-28 23:59:28 +0100

    🚧 Fetch a particular user's ride histories - I

--- src/controllers/rideController.js
+++ src/controllers/rideController.js
@@ -1,7 +1,28 @@
+const models = require("../models")
+const User = models.User
+const Ride = models.Ride
 const bookRideController = async (req, res) => {
 	res.status(200).json({ success: true })
 }
 
+const getUserRidesController = async (req, res) => {
+	try {
+		const { user } = req
+
+		const rides = await Ride.find({ user: user._id })
+
+		if (rides.length === 0) {
+			return res.status(404).json({ error: "No rides available. Book now" })
+		}
+
+		return res.status(200).json({ rides })
+	} catch (error) {
+		console.error("Error in getLocationsByZoneController:", error)
+		return res.status(500).json({ error: "Internal server error" })
+	}
+}
+
 module.exports = {
 	bookRideController,
+	getUserRidesController,
 }
--- src/routes/rideRoute.js
+++ src/routes/rideRoute.js
@@ -6,6 +6,7 @@ const middlewares = require("../middlewares")
 const { protectRoute } = middlewares
 
 router.post("/book-ride", protectRoute, rideController.bookRideController)
+router.get("/get-user-rides", protectRoute, rideController.getUserRidesController)
 // router.post("/cancel-ride", rideController.loginController)
 
 module.exports = router
