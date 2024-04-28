3b30721a864dd8032ebec66fb4c54ea3d46fa098
Theophilus <theophilusadesola002@gmail.com>
2024-04-28 14:22:07 +0100

    User authorization routes/API/controller implemented.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -80,7 +80,24 @@ const logoutController = async (req, res) => {
 	}
 }
 
+const authorizeUserController = async (req, res) => {
+	try {
+		const { user } = req
+
+		if (!user) return res.status(400).json({ error: "Unauthorized" })
+		if (user.isSuspended) return res.status(400).json({ error: "Account suspended" })
+
+		return res.status(200).json({
+			message: "Authorized",
+		})
+	} catch (error) {
+		console.log("Error in authorize user controller", error.message)
+		return res.status(500).json({ error: "Internal server error" })
+	}
+}
+
 module.exports = {
+	authorizeUserController,
 	loginController,
 	registerController,
 	logoutController,
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,8 +3,12 @@ const router = express.Router()
 const controllers = require("../controllers")
 const authController = controllers.authController
 
+const middlewares = require("../middlewares")
+const protectRoute = middlewares.protectRoute
+
 router.post("/register", authController.registerController)
 router.post("/login", authController.loginController)
 router.post("/logout", authController.logoutController)
+router.get("/authorize-user", protectRoute, authController.authorizeUserController)
 
 module.exports = router
