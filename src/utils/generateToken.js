a83e540f0303f9c3483bcbc32b0d6ae5783b2c66
Theophilus Adeleke <theophilusadesola002@gmail.com>
2024-06-06 18:46:24 +0100

    🚑 Token generation settings reconfigured.

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,16 +1,16 @@
-const jwt = require("jsonwebtoken")
+const jwt = require( "jsonwebtoken" )
 
-const generateToken = async (userId, res) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "3d",
-	})
+const generateToken = async ( userId, res ) => {
+    const token = jwt.sign( {userId}, process.env.JWT_SECRET, {
+        expiresIn: "3d",
+    } )
 
-	res.cookie("jwt", token, {
-		maxAge: 3 * 24 * 60 * 60 * 1000,
-		httpOnly: true,
-		sameSite: "strict",
-		secure: process.env.NODE_ENV === "development" ? false : true,
-	})
+    res.cookie( "jwt", token, {
+        maxAge: 3 * 24 * 60 * 60 * 1000,
+        httpOnly: false,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "development" ? false : true,
+    } )
 }
 
 module.exports = generateToken
